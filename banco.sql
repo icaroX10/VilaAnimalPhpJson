675362b44cfe4706dbb1dbb36189f078dd21c137
icaroX10 <icaro_music@hotmail.com>
2017-12-11 12:22:40 -0300

    banco mudado

--- banco.sql
+++ banco.sql
@@ -1,122 +1,34 @@
--- phpMyAdmin SQL Dump
--- version 4.5.3.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 11-Dez-2017 às 03:20
--- Versão do servidor: 5.7.10
--- PHP Version: 5.6.17
+Create table telefonepf(
+	id_telpf int(11) not null auto_increment primary key,
+	telefone_id int(11) not null,
+	pessoaf_id int(11) not null,
+	foreign key (telefone_id) references telefone (id),
+	foreign key (pessoaf_id) references pessoaf (id_pes_f)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+Create table telefonepj(
+	id_telpj int(11) not null auto_increment primary key,
+	telefone_id int(11) not null,
+	pessoaj_id int(11) not null,
+	foreign key (telefone_id) references telefone (id),
+	foreign key (pessoaj_id) references pessoaj (id_pes_j)
+);
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `vilaanimal`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `dados`
---
-
-CREATE TABLE `dados` (
-  `id` int(11) NOT NULL,
-  `pontuacao` double NOT NULL,
-  `tempo` double NOT NULL,
-  `data` varchar(30) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `dados`
---
+Create table enderecopf(
+	id_endpf int(11) not null auto_increment primary key,
+	endereco_id int(11) not null,
+	pessoaf_id int(11) not null,
+	foreign key (endereco_id) references endereco (id),
+	foreign key (pessoaf_id) references pessoaf (id_pes_f)
+);
 
 
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `paciente`
---
-
-CREATE TABLE `paciente` (
-  `id` int(11) NOT NULL,
-  `nickname` varchar(60) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `paciente`
---
+Create table enderecopj(
+	id_endpj int(11) not null auto_increment primary key,
+	endereco_id int(11) not null,
+	pessoaj_id int(11) not null,
+	foreign key (endereco_id) references endereco (id),
+	foreign key (pessoaj_id) references pessoaj (id_pes_j)
+);
 
 
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `paciented`
---
-
-CREATE TABLE `paciented` (
-  `id_pacd` int(11) NOT NULL,
-  `paciente_id` int(11) NOT NULL,
-  `dados_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `paciented`
---
-
-
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `dados`
---
-ALTER TABLE `dados`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `paciente`
---
-ALTER TABLE `paciente`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `paciented`
---
-ALTER TABLE `paciented`
-  ADD PRIMARY KEY (`id_pacd`),
-  ADD KEY `paciente_id` (`paciente_id`),
-  ADD KEY `dados_id` (`dados_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `dados`
---
-ALTER TABLE `dados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
---
--- AUTO_INCREMENT for table `paciente`
---
-ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `paciented`
---
-ALTER TABLE `paciented`
-  MODIFY `id_pacd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
